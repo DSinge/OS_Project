4f16ffcfd636564f4c3b355c6f5c166b7d475924
Stephan Kritikos <skny93@me.com>
2015-12-08 12:26:15 -0500

    Rework
    Changed to Daniel's code with my scheduler added in. Scheduler currently
    unfixed. Just wanted to get it up first

--- OS_Project/mainDriver.cpp
+++ OS_Project/mainDriver.cpp
@@ -1,169 +1,368 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <list>
+#include <queue>
+#include <iterator>
+using namespace std;
 
-#include "memoryManager.h"
-#include "scheduler.h"
-#include "IO_Manager.h"
-
-std::vector< std::vector<int> > storedJobs; // storage of jobs not in working memory. Sorted by priority when sorted.	Format: vector pos, job array(whole p[5] array)
-std::vector< std::vector<int> > readyJobs; //storage of positions in memory of ready to be scheduled jobs.				Format: priority, position in memory
-std::vector< std::vector<int> > memBreaks; //positions in memory where space is availible.								Format: start position, size of free memory
-bool memFull;
-
-// Channel commands siodisk and siodrum are made available to you by the simulator.
-// siodisk has one argument: job number, of type int and passed by value.
-// siodrum has four arguments, all of type int and passed by value:
-	// first argument is job number;
-	// second argument is job size;
-	// third argument is starting core address;
-	// fourth argument is interpreted as follows:
-		// 1 => move from core (memory) to drum
-		// 0 => move from drum to core (memory)
-
+void siodisk(int jobnum);
+void siodrum(int jobnum, int jobsize, int coreaddress, int direction);
 void ontrace(); // called without arguments
 void offtrace(); // called without arguments
 
-// The 2 trace procedures allow you to turn the tracing mechanism on and off.
-// The default value is off. WARNING: ontrace() produces a blow-by-blow description
-// of each event and results in an extremely large amount of output.
-// It should be used only as an aid in debugging.
-// Even with the trace off, performance statistics are
-// generated at regular intervals and a diagnostic message appears in case of a crash.
-// In either case, your OS need not print anything.
+// Daniel
+// Holds any relevant info about a job
+struct PCB {
+    int jobNumber;
+    int priority;
+    int jobSize;
+    int maxCpuTime;
+    int arrivalTime;
+    int cpuTimeUsed;
+	int memoryPos;
+
+    bool inCore;
+    
+    bool onIOQueue;
+    bool isDoingIO;
+
+};
+// Stores all jobs
+// Index is a job number and value is a PCB
+static map<int, PCB> jobTable;
+
+// Daniel
+// Stores all the free spaces in memory
+// in the form <Starting address, Size of free space>
+// New nodes are created as needed
+static list<pair<int, int> > freeSpaceTable;
+
+// Stores pointers to PCBs in jobTable
+// Using pointers to easily test if the job is in memory
+static queue<PCB*> IOQueue;
+
+//Stores the current time, taken from interrupts
+static int time;
+
+bool jobInCPU;
+
+void allocMem(int startAddress, int jobSize);
+int findMemLoc(int jobSize);
+
+//Finds the highest priority job stored in jobTable and returns the job (key) number
+unsigned int findJob();
+
+bool scheduler(int &a, int p[]);
 
 
+/* Function: startup
+ * -----------------
+ * Allows initialization of (static) system variables declared above.
+ * Called once at start of the simulation.
+ */
 
-void startup()
-
-{
-	ontrace();
-	memFull = false;
-	IO_ManagerInitialization();
-	memBreaks.push_back({ 0, 99 }); //Initial memory tracker set to have one large portion between 0-99
-
-	// Allows initialization of (static) system variables declared above
-	// Called once at start of the simulation.
+void startup(){
+    
+    //ontrace();
+    // First node - All of memory is free
+    freeSpaceTable.push_back(pair<int, int>(0, 99));
+    time = 0;
+	jobInCPU = false;
 }
+
+
 
 // INTERRUPT HANDLERS
 // The following 5 functions are the interrupt handlers. The arguments
 // passed from the environment are detailed with each function below.
 // See RUNNING A JOB, below, for additional information
 
-void Crint(int &a, int p[])
-
-{
+/* Function: Crint
+ * ---------------
+ * Indicates the arrival of a new job on the drum.
+ * At call : p [1] = job number
+ * p [2] = priority
+ * p [3] = job size, K bytes
+ * p [4] = max CPU time allowed for job
+ * p [5] = current time
+ */
+void Crint(int &a, int p[]){
 	std::cout << "Crint called\n";
-	swapper(p, memBreaks, readyJobs, storedJobs);
 
-	// Indicates the arrival of a new job on the drum.
-	// At call : p [1] = job number
-	// p [2] = priority
-	// p [3] = job size, K bytes
-	// p [4] = max CPU time allowed for job
-	// p [5] = current time
+    // Create a PCB and store it in the job table
+    // Maybe this should be a constructor?
+    // I think it's clearer this way, but that could be argued
+    PCB pcb;
+    pcb.jobNumber = p[1];
+    pcb.priority = p[2];
+    pcb.jobSize = p[3];
+    pcb.maxCpuTime = p[4];
+    pcb.arrivalTime = p[5];
+    pcb.cpuTimeUsed = 0;
+    pcb.inCore = false; // Will be set in Drmint
+    pcb.onIOQueue = false;
+    pcb.isDoingIO = false;
+	pcb.memoryPos = NULL;
+
+    int memLoc = findMemLoc(p[3]);
+
+    if(memLoc >= 0){
+        allocMem(memLoc, p[3]);
+		pcb.memoryPos = memLoc;
+        siodrum(p[1], p[3], memLoc, 0);
+    }
+    else{
+        // Swap a job out
+        exit(0); // Temporary
+    }
+
+	jobTable.insert(pair<int, PCB>(p[1], pcb));
+
+    time = p[5];
+	cout << "Time = " << time << "\n\n";
 }
 
 
-void Dskint(int &a, int p[])
 
-{
+/* Function: Dskint
+ * ----------------
+ * Disk interrupt.
+ * At call: p[5] = current time
+ */
+void Dskint(int &a, int p[]){
 	std::cout << "Dskint called\n";
-	// Disk interrupt.
-	// At call : p [5] = current time
+
+    printf(" CPU time used: %d\n", jobTable[1].cpuTimeUsed);
+    IOQueue.pop();
+	jobTable[p[1]].isDoingIO = false;
+    
+    if(!IOQueue.empty()){
+        printf("IOQueue not empty");
+    }
+
+	if (jobInCPU == false)
+		scheduler(a, p);
+
+    time = p[5];
+	cout << "Time = " << time << "\n\n";
 
 }
 
-/*
-Interupt: When drum is finished moving something in or out of memory, this is called
+
+
+/* Function: Drmint
+ * ----------------
+ * Drum interrupt. When drum is finished moving something in or out of memory, this is called
+ * At call : p [5] = current time
+ */
+void Drmint(int &a, int p[]){
+	cout << "Drmint Called\n";
+	jobTable[p[1]].inCore = true;
+
+	//Only scheduling here if there's absolutely nothing in the CPU
+	if (jobInCPU == false)
+		scheduler(a, p);
+
+  //  printf("p[4]: %d\n", p[4]);
+
+    time = p[5];    
+	cout << "Time = " << time << "\n\n";
+}
+
+
+
+/* Function: Svc
+ * -------------
+ * Supervisor call from user program.
+ * At call : p [5] = current time
+ * a = 5 => job has terminated
+ * a = 6 => job requests disk i/o
+ * a = 7 => job wants to be blocked until all its pending
+ * I/O requests are completed
+ */
+void Svc(int &a, int p[]){
+	std::cout << "Svc called\n";
+
+	jobTable[p[1]].cpuTimeUsed += p[5] - time;
+	printf(" Job %i CPU time used: %d\n", p[1], jobTable[p[1]].cpuTimeUsed);
+    
+    
+    switch(a){
+        case 5:    // Job has terminated
+        {
+			jobTable.erase(p[1]);
+			if (scheduler(a, p) == false) //Scheduler will run, but if it returns 0, that means there is nothing that can be scheduled, so it will stall the CPU
+			{
+				jobInCPU = false;
+				a = 1;
+			}
+			printf(" Job %i has terminated\n", p[1]);
+			time = p[5];
+        } break;
+        
+        case 6:    // Job requests disk I/O
+        {//Only handling first item
+			printf(" Job %i requested I/O\n", p[1]);
+
+            IOQueue.push(&jobTable[p[1]]);
+            jobTable[p[1]].onIOQueue = true;
+            //cout << IOQueue.front()->jobNumber << endl;
+
+            siodisk(IOQueue.front()->jobNumber);
+            jobTable[IOQueue.front()->jobNumber].isDoingIO = true;
+
+            a = 2;
+            p[2] = 0;
+            p[3] = 18;
+            p[4] = jobTable[1].maxCpuTime - jobTable[1].cpuTimeUsed;
+            time = p[5];
+        } break;
+        
+        case 7:    // Job wants to be blocked
+        {
+			printf(" Job %i wants to be blocked\n", p[1]);
+			if (scheduler(a, p) == false) //Scheduler will run, but if it returns 0, that means there is nothing that can be scheduled, so it will stall the CPU
+			{
+				jobInCPU = false;
+				a = 1;
+			}
+			time = p[5];
+        } break;
+
+        default:
+        {
+            printf("a has invalid value %d\n", a);
+        }
+    }
+	cout << "Time = " << time << "\n\n";
+}
+
+/* Function: Tro
+* -------------
+* Timer-Run-Out.
+* At call : p [5] = current time
 */
-void Drmint(int &a, int p[])
+void Tro(int &a, int p[]){
 
-{
-	std::cout << "Drmint called\n";
-	a = 2; //telling the CPU to become active after each memory movement may start problems, check in later
-
-	//sort before scheduling
-	sortReadyJobs(0, readyJobs.size() - 1, readyJobs);
-	scheduler(p, memBreaks, readyJobs, storedJobs);
-
-	// Drum interrupt.
-	// At call : p [5] = current time
-
-}
-
-
-
-void Tro(int &a, int p[])
-
-{
 	std::cout << "Tro called\n";
-	// Timer-Run-Out.
-	// At call : p [5] = current time
 
+	time = p[5];
+	cout << "Time = " << time << "\n\n";
 }
 
 
-void Svc(int &a, int p[])
+//End of Interrupts
+//===================
+//Start of Functions
 
-{
-	std::cout << "Scv called\n";
-	// Supervisor call from user program.
-	// At call : p [5] = current time
-	// a = 5 => job has terminated
-	// a = 6 => job requests disk i/o
-	// a = 7 => job wants to be blocked until all its pending
-	// I/O requests are completed
 
-	switch (a)
+
+/* Function: allocMem
+* ------------------
+* Updates the Free Space Table when a job is placed in memory
+*/
+void allocMem(int startAddress, int jobSize){
+
+	list<pair<int, int> >::iterator freeSpaceIter;
+
+	for (freeSpaceIter = freeSpaceTable.begin();
+		freeSpaceIter != freeSpaceTable.end();
+		freeSpaceIter++)
 	{
-	case 5 :
-		//terminate
-		break;
-	case 6 :
-		diskQueueManager(p);
-		break;
-	case 7 :
-		//Block job
-		break;
+		if (freeSpaceIter->first == startAddress){
+			freeSpaceIter->first += jobSize;
+			freeSpaceIter->second -= jobSize;
+			if (freeSpaceIter->second == 0){
+				freeSpaceTable.erase(freeSpaceIter);
+				return;
+			}
+		}
+	}
+}
+
+
+
+/* Function: findMemLoc
+ * --------------------
+ * First-Fit Algorithm
+ * Looks in the Free Space Table for the first space
+ * that is >= jobSize
+ *
+ * returns: the address of a valid space (0-99)
+ *          -1 if no space is >= jobSize
+ */
+int findMemLoc(int jobSize){
+
+    list<pair<int, int> >::iterator freeSpaceIter;
+
+    for(freeSpaceIter = freeSpaceTable.begin();
+        freeSpaceIter != freeSpaceTable.end();
+        freeSpaceIter++)
+    {
+        if(freeSpaceIter->second >= jobSize){
+            return freeSpaceIter->first;
+        }
+    }
+    return -1;
+    
+}
+
+
+
+/* Function: findJob
+* ---------------
+* Sets up an iterator to check the jobTable map
+* While the first element of the map is not in core, it will keep changing the starting values until it finds one that is
+* Afterwards, it checks the remaining members of jobTable for any that might have higher priority
+* Returns the position of a high priority item
+*/
+unsigned int findJob()
+{
+	unsigned int highPriJobPos = 1, highPriJobPriority = jobTable[1].priority;
+
+	for (unsigned int i = 1; i < jobTable.size(); i++)
+	{
+		if (jobTable[i].priority < highPriJobPriority && jobTable[i].inCore == true && jobTable[i].isDoingIO == false) //Is the priority on the node it's looking at and is it in the core?
+		{
+			highPriJobPos = jobTable[i].jobNumber;
+			highPriJobPriority = jobTable[i].priority;
+		}
 	}
 
+	//Here to make sure the default isn't breaking the rules
+	if (jobTable[highPriJobPos].isDoingIO == true)
+	{	
+		printf(" No suitable job could be found!\n");
+		return NULL;
+	}
+	return highPriJobPos;
 }
 
 
 
-
-//Additional functions local to OS(scheduler(), swapper(), etc.)
-//RUNNING A JOB :
-
-// Before leaving each interrupt handler (with the return statement)
-// you must set the a and p[] arguments as follows:
-
-// a = 1 CPU is idle, p is ignored
-// a = 2 CPU is in user mode,
-
-// p [0], p [1], and p [5] are ignored
-// p [2] = base address of job to be run
-// p [3] = size (in K) of job to be run
-// p [4] = time quantum
-
-
-/*
-------------------------------------------------------------------------------------------------------------------
-
-NOTES:
-
--time is in milliseconds.
-
-- core addresses are in K(0 - 99).
-
-- priority ranges from 1 (highest)to 10 (lowest).
-
-- assume interrupts are inhibited while OS is executing.
-
-
-TO RUN SOS WITH YOUR OS :
-compile yourfile.cpp separately and link with sos.obj(PC) or sos.o(Unix).
-main() is defined in sos.
-
+/* Function: scheduler
+* --------------------
+* Tells the CPU which job to process
+* Highest Priority chosen using 
+*  
 */
+bool scheduler(int &a, int p[])
+{	 
+	int pos = findJob();
+	if (pos == NULL)
+		return false;
+
+	printf(" Placing Job %i into the CPU\n", pos);
+
+	p[1] = jobTable[pos].jobNumber;
+	p[2] = jobTable[pos].memoryPos;
+	p[3] = jobTable[pos].jobSize;
+	p[4] = jobTable[pos].maxCpuTime - jobTable[pos].cpuTimeUsed;
+	cout << " p[4]= " << p[4] << "\n";
+
+	a = 2;
+	jobInCPU = true;
+
+	return true;
+}
--- OS_Project/scheduler.h
+++
@@ -1,8 +0,0 @@
-#ifndef SCHEDULER_H
-#define SCHEDULER_H
-
-#include<vector>
-
-void scheduler(int *p, std::vector< std::vector<int> > &memBreaks, std::vector< std::vector<int> > &readyJobs, std::vector< std::vector<int> > &storedJobs);
-
-#endif
